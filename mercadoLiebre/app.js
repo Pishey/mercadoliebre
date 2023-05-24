dd79bf20867a48442894283aa25d346f78e3958f
Lautaro Andrades <lautiurielandrades@gmail.com>
2023-05-24 16:19:31 -0300

    nana

--- mercadoLiebre/app.js
+++ mercadoLiebre/app.js
@@ -12,10 +12,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
 });
 
-app.get('login', (req, res) => {
+app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/login.html'))
 });
 
-app.get('register', (req, res) => {
+app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/register.html'))
 });
